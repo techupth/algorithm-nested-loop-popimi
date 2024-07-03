4685150c1e04da3da0f215e02c4849b0db9b7c91
Pop <shounen.pop@gmail.com>
2024-07-03 11:06:39 +0700

    feat: add answer ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,4 +1,16 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printPattern() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printPattern(num) {
+  let result = "";
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (j === i) {
+        result += j + "\n";
+      } else result += j + "\t";
+    }
+  }
+  return result;
 }
+
+const num = 5;
+
+console.log(printPattern(num));
